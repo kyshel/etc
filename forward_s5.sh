3cacf34315a426b1c64385545abb8099c0128a94
root <root@localhost.localdomain>
2020-07-12 08:20:59 +0000

    add ubuntu support

--- forward_s5.sh
+++ forward_s5.sh
@@ -11,21 +11,21 @@
 # 1. >>> yum -y install privoxy <<<
 # 2. >>> cp /etc/privoxy/config /etc/privoxy/config_bak <<<
 # 3. vi above, enable this: >>> forward-socks5t / 127.0.0.1:1080 . <<<
-
+# 4. if OS is ubuntu, cahnge systemctl to service
 
 
 
 
 if [ "$1" == "on" ]; then
-    service privoxy start
+    systemctl  start privoxy
     export http_proxy=http://127.0.0.1:8118
     export https_proxy=http://127.0.0.1:8118
 elif [ "$1" == "off" ]; then
-    service privoxy stop
+    systemctl  stop   privoxy
     export http_proxy=""
     export https_proxy=""
 elif [ "$1" == "status" ]; then
-    service privoxy status
+    systemctl  status privoxy
 elif [ "$1" == "log" ]; then
     echo 'parameter not set'
 elif [ "$1" == "test" ]; then
